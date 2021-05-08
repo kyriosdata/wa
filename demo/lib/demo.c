7f81f03990bbc76e9367a6384188afc6e5994e1e
kyriosdata <fabio@inf.ufg.br>
2021-05-08 12:25:03 -0300

    doc

--- demo/lib/demo.c
+++ demo/lib/demo.c
@@ -1,10 +1,14 @@
 #include <emscripten.h>
 #include <stdio.h>
 
+char* mensagem = "Olá, seja bem-vindo ao mundo!";
+
 int main() {
     printf("Main!\n");
     int valorInt = emscripten_run_script_int("getNum()");
+    
     printf("Valor obtido de código em JavaScript: %d\n", valorInt);
+    printf("%s\n", mensagem);
     return -95;
 }
 
