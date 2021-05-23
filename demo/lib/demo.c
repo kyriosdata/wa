2fcd89c642d14a77d4b5b95fa51eca0e06ffeada
kyriosdata <fabio@inf.ufg.br>
2021-05-23 19:11:00 -0300

    busca binaria

--- demo/lib/demo.c
+++ demo/lib/demo.c
@@ -1,21 +1,52 @@
-#include <emscripten.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-char* mensagem = "Olá, seja bem-vindo ao mundo!";
+int buscaBinaria(char *palavras[], int tamanho, char *termo)
+{
+    int bottom = 0;
+    int mid;
+    int top = tamanho - 1;
+    int comparacao = 0;
 
-int main() {
-    printf("Main!\n");
-    int valorInt = emscripten_run_script_int("getNum()");
-    
-    printf("Valor obtido de código em JavaScript: %d\n", valorInt);
-    printf("%s\n", mensagem);
-    return -95;
+    while (bottom <= top)
+    {
+        mid = (bottom + top) / 2;
+        comparacao = strcmp(palavras[mid], target);
+        if (comparacao == 0)
+        {
+            return mid;
+        }
+
+        if (comparacao > 0)
+        {
+            top = mid - 1;
+        }
+        else
+        {
+            bottom = mid + 1;
+        }
+    }
+
+    return -1;
 }
 
-int numero() {
-    return 234;
-}
+int main(int argc, char *argv[])
+{
+    int i = 1;
+    char *input = "talk";
 
-int quadrado(int x) {
-    return x * x;
+    char *verbs[5] = {"do", "make", "shit", "talk", "walk"};
+
+    printf("looking at %s\n", input);
+    int index = buscaBinaria(verbs, 5, input);
+    if (index < 0)
+    {
+        printf("Nao encontrado\n");
+    }
+    else
+    {
+        printf("Encontrado em %d\n", index);
+    }
+    return 0;
 }
